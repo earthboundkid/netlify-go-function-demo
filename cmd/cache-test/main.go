4a054392d0a4052a2db04d21ff7ae76bed9ec9ee
Carl Johnson <me@carlmjohnson.net>
2019-10-11 15:01:15 -0400

    Testing

---
+++ cmd/cache-test/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	now := time.Now()
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       fmt.Sprintf("Hello, World %s", now.Format("2006-01-02 3:05:06pm")),
+		Headers: map[string]string{
+			"Cache-Control": "public, max-age=31536000",
+		},
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/carlmjohnson/netlify-function-cache-test
+
+go 1.13
+
+require github.com/aws/aws-lambda-go v1.13.2
