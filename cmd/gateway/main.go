3b6479c09d000e9d2c9424ca06ae7566246858a2
Carl Johnson <me@carlmjohnson.net>
2020-03-06 14:32:56 -0500

    Handler: Remove echo, better name for middleware

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
 
 	"github.com/apex/gateway"
 	"github.com/carlmjohnson/feed2json"
@@ -22,24 +21,13 @@ func main() {
 		http.Handle("/", http.FileServer(http.Dir("./public")))
 	}
 
-	http.HandleFunc("/api/echo", echo)
 	http.Handle("/api/feed", feed2json.Handler(
-		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"), nil, nil, nil, addCache))
+		feed2json.StaticURLInjector("https://news.ycombinator.com/rss"),
+		nil, nil, nil, cacheControlMiddleware))
 	log.Fatal(listener(portStr, nil))
 }
 
-func echo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Header().Set("Cache-Control", "public, max-age=60")
-	b, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		log.Printf("could not dump request: %v", err)
-		return
-	}
-	w.Write(b)
-}
-
-func addCache(h http.Handler) http.Handler {
+func cacheControlMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=300")
 		h.ServeHTTP(w, r)
