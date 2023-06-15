d1e643ed5ca4e51c5c3417b674040a483565ffa1
Carl Johnson <me@carlmjohnson.net>
2023-06-15 10:21:09 -0400

    restore branch

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -euxo pipefail
 
 mkdir -p "$(pwd)/functions"
-GOBIN=$(pwd)/functions go install github.com/spotlightpa/almanack/funcs/almanack-api@latest
+GOBIN=$(pwd)/functions go install ./cmd/...
 chmod +x "$(pwd)"/functions/*
 go env
