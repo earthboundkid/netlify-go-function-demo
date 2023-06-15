c44164238fda0e0d4e51a7568b44770213b9e474
Carl Johnson <me@carlmjohnson.net>
2023-06-14 21:44:16 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -euxo pipefail
 
 mkdir -p "$(pwd)/functions"
-GOBIN=$(pwd)/functions go install ./...
+GOBIN=$(pwd)/functions go install github.com/spotlightpa/almanack/funcs/almanack-api@latest
 chmod +x "$(pwd)"/functions/*
 go env
