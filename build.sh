a748c8f8dd554fd4012601b7ed50dc08dfc41d66
Carlana <mail@carlana.net>
2025-10-09 13:37:01 -0400

    Spaghetti

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$THIS_DIR"
 
 mkdir -p "$THIS_DIR/functions"
-LDFLAGS="-linkmode external -extldflags "-static""
+LDFLAGS="-extldflags "-static""
 CGO_ENABLED=0 GOBIN=$THIS_DIR/functions go install -ldflags "$LDFLAGS" ./...
 chmod +x "$THIS_DIR"/functions/*
 go env
